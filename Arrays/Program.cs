9c3c3c0ce667ecf75c1e3581ccb755c67349c77e
Gokhan <Gokhan@DESKTOP-4K38GM2>
2022-12-06 16:17:26 +0300

    Arrays.

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Arrays
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
