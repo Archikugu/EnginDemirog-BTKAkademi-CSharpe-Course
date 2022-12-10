980461d97ac8ad64f1655054945f5bfc48de54a7
Gokhan <engokhangok@gmail.com>
2022-12-10 13:57:58 +0300

    Classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Customer
+    {
+        //field
+        //public string FirstName;
+
+        //Property
+        public int CustomerId { get; set; }
+
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get
+        //    {
+        //        return "Mr." + _firstName;
+        //    }
+        //    set
+        //    {
+        //        _firstName = value;
+        //    }
+        //}
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.CustomerId = 1;
+            customer.FirstName = "Gokhan";
+            customer.LastName = "Gok";
+            customer.City = "Ankara";
+
+            Customer customer2 = new Customer { CustomerId = 2, FirstName = "Gokhan", LastName = "Gok", City = "Istanbul" };
+
+          
+            Console.WriteLine(customer2.FirstName);
+
+        }
+    }
+}
