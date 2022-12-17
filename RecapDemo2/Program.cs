edf0c285c4df40640b2843af91dc08d63505d3dc
Gokhan <engokhangok@gmail.com>
2022-12-17 14:32:13 +0300

    RecapDemo2 (Log Operations)

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger();
+            customerManager.Logger = new Filelogger();
+            customerManager.Logger = new Smslogger();
+
+            Smslogger smslogger = new Smslogger();
+            smslogger.Log();
+
+
+            customerManager.Add();
+        }
+    }
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer Added!");
+        }
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+    class Filelogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+    }
+    class Smslogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+}
