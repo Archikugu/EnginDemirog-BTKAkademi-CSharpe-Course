6aafca235555ecab03a7ef6e8fc91d98545c80b5
Gokhan <engokhangok@gmail.com>
2022-12-17 14:18:42 +0300

    Abstract Classes

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new OracleServer();
+            database.Add();
+            database.Delete();
+
+            Database database2=new SqlServer();
+            database2.Add();
+            database2.Delete();
+        }
+    }
+    //Abstract class = virtual methods + interface 
+    //Abstract class = inheritance dır 
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public abstract void Delete();
+    }
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+    class OracleServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
